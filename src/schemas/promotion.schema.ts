7f49826f80fe744e60fa1dc6df99b6b0228d074a
Niyi <obikoya11@gmail.com>
2019-10-09 11:21:52 +0100

    wrote promotion schema

--- src/schemas/promotion.schema.ts
+++ src/schemas/promotion.schema.ts
@@ -1,1 +1,22 @@
 import * as mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+export const PromotionSchema = new Schema({
+    postId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    promoter: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    medium: {
+        type: String,
+        enum: ['twitter', 'whatsapp', 'facebook', 'linkedin', 'instagram'],
+        required: true
+    }
+}, {
+    timestamps: true
+});
